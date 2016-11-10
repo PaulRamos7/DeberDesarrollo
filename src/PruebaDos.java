9d707a8cc363bdbe4c174263af9c686d3cd9c7b6
ManzanoRodrigo <esparquiwho@outlook.com>
2016-11-10 12:19:09 -0500

    manzano paul

--- src/PruebaDos.java
+++ src/PruebaDos.java
@@ -3,13 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package desarrollo;
 
 /**
  *
- * @author vane
+ * @author Esparqui
  */
-public class vane {
-   //SDF
- 
+public class PruebaDos {
+    
 }
